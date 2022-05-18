3d771f98f0d30dfb7baa8314954c48c69ed8c912
arymoraes <arylmoraesn@hotmail.com>
2022-05-18 17:18:05 -0600

    mobile menu

--- components/Header/CpHeader.tsx
+++ components/Header/CpHeader.tsx
@@ -1,17 +1,77 @@
+import { Menu, Text } from '@mantine/core';
 import type { NextPage } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import Logo from '../../assets/taishi.svg';
 import styles from './CpHeader.module.scss';
 
+const menuOptions = [
+  {
+    href: '/',
+    label: 'Início',
+  },
+  {
+    href: '/about',
+    label: 'Quem somos',
+  },
+  {
+    href: '/services',
+    label: 'Serviços',
+  },
+  {
+    href: '/light-steel-frame',
+    label: 'Light Steel Frame',
+  },
+  {
+    href: '/blog',
+    label: 'Blog',
+  },
+  {
+    href: '/contato',
+    label: 'Contato',
+  },
+];
+
 const CpHeader: NextPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
     <nav className={styles.header}>
       <div>
         <Image height={100} src={Logo} width={100} />
       </div>
       <div>
-        <GiHamburgerMenu className={styles.menu} size={40} />
+        <Menu
+          control={
+            <button className={styles.menuPressable}>
+              {isOpen ? (
+                <AiOutlineClose className={styles.menu} size={40} />
+              ) : (
+                <GiHamburgerMenu className={styles.menu} size={40} />
+              )}
+            </button>
+          }
+          onClose={toggleDrawer}
+          onOpen={toggleDrawer}
+          opened={isOpen}
+          transitionDuration={200}
+          transitionTimingFunction="ease"
+        >
+          {menuOptions.map((option) => (
+            <Menu.Item key={option.label}>
+              <Link href={option.href}>
+                <Text size="lg">{option.label}</Text>
+              </Link>
+            </Menu.Item>
+          ))}
+        </Menu>
       </div>
     </nav>
   );
