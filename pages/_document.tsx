4e45a2cdaf211cdd70004d0e519c18553b4eed7f
Ary <arylmoraesn@gmail.com>
2022-07-26 15:42:43 -0600

    google search console

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,33 +1,26 @@
-import Document, {
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from 'next/document';
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link
-            href="https://fonts.googleapis.com"
-            rel="preconnect"
-          />
+          <link href="https://fonts.googleapis.com" rel="preconnect" />
           <link href="https://fonts.gstatic.com" rel="preconnect" />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Montserrat:wght@400;500;900&display=swap"
             rel="stylesheet"
           />
-          <link
-            href="https://fonts.googleapis.com"
-            rel="preconnect"
-          />
+          <link href="https://fonts.googleapis.com" rel="preconnect" />
           <link href="https://fonts.gstatic.com" rel="preconnect" />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Montserrat:wght@400;500;900&family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           ></link>
+          <meta
+            name="google-site-verification"
+            content="FCxb0a-WMswxIF9cBTT1GJX3ymSlZmcyxNMJq4o0lLQ"
+          />
         </Head>
         <body>
           <Main />
