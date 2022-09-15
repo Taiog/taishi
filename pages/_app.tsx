c6bbae02e8cc7f6145e478697d64c908ebf61ab1
Ary <arylmoraesn@gmail.com>
2022-09-15 17:25:42 -0600

    GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,36 @@
-import { MantineProvider } from '@mantine/core';
-import type { AppProps } from 'next/app';
-import CpFooter from '../src/components/Footer/CpFooter';
-import CpHeader from '../src/components/Header/CpHeader';
-import CtLang from '../src/containers/CtLang';
-import '../styles/globals.css';
+import { MantineProvider } from "@mantine/core";
+import type { AppProps } from "next/app";
+import ReactGA from "react-ga4";
+import CpFooter from "../src/components/Footer/CpFooter";
+import CpHeader from "../src/components/Header/CpHeader";
+import CtLang from "../src/containers/CtLang";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  if (process.env.PROD && process.env.GA_ID) {
+    ReactGA.initialize(process.env.GA_ID);
+  }
+
   return (
     <MantineProvider
       theme={{
         // Theme is deeply merged with default theme
-        colorScheme: 'light',
+        colorScheme: "light",
         colors: {
           // Add your color
-          'deep-blue': ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
+          "deep-blue": ["#E9EDFC", "#C1CCF6", "#99ABF0" /* ... */],
           // or replace default theme color
-          blue: ['#E9EDFC', '#C1CCF6', '#99ABF0' /* ... */],
+          blue: ["#E9EDFC", "#C1CCF6", "#99ABF0" /* ... */],
         },
 
         shadows: {
           // other shadows (xs, sm, lg) will be merged from default theme
-          md: '1px 1px 3px rgba(0,0,0,.25)',
-          xl: '5px 5px 3px rgba(0,0,0,.25)',
+          md: "1px 1px 3px rgba(0,0,0,.25)",
+          xl: "5px 5px 3px rgba(0,0,0,.25)",
         },
 
         headings: {
-          fontFamily: 'Montserrat, sans-serif',
+          fontFamily: "Montserrat, sans-serif",
           sizes: {
             h1: { fontSize: 30 },
           },
